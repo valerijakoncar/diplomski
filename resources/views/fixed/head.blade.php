719a976d5d75d6a92645e0965cc6e38aed3b0b2f
valerijakoncar <valerijakoncar20@gmail.com>
2021-01-14 23:08:18 +0100

    addedHeadMetas

--- resources/views/fixed/head.blade.php
+++ resources/views/fixed/head.blade.php
@@ -3,12 +3,15 @@
 <head>
     <title>MovieBlackout - @yield("title")</title>
     <meta charset="utf-8"/>
-{{--    <meta name="description" content="">--}}
-{{--    <meta name="keywords" content=""/>--}}
-{{--    <meta name="author" content="">--}}
+    <meta name="description" content="Watch newest movies with your friends and family, all while enjoying best popcorns you've ever tried.">
+    <meta name="keywords" content="Cinema, watch, movies, tickets"/>
+    <meta name="author" content="mailto:valerijakoncar20@gmail.com">
     <link rel="shortcut icon" href="{{asset('images/edited/popcorn.ico')}}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="language" content="english"/>
+    <meta name="copyright" content="MovieBlackout Cinema"/>
+    <meta name="robots" content="index, follow"/>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}"/>
