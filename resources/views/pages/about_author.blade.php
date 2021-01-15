2ba9bb9f049c0678ceb32f4f6eeaea6ee60c1d04
valerijakoncar <valerijakoncar20@gmail.com>
2021-01-15 18:55:50 +0100

    dateOption

--- resources/views/pages/about_author.blade.php
+++ resources/views/pages/about_author.blade.php
@@ -1,21 +1,21 @@
 @extends("template")
 @section("title")
-About Author
+    About Author
 @endsection
 @section("mainContent")
-<div id="centeredAboutAuthor">
-    <h2>About Author</h2>
-    <div class="decorationalDiv">
-        <span></span>
-    </div>
-    <div id="author">
-        <div id="picAuthor">
-            <img src="{{asset('images/me.png')}}" alt="author"/>
+    <div id="centeredAboutAuthor">
+        <h2>About Author</h2>
+        <div class="decorationalDiv">
+            <span></span>
         </div>
-        <div id="authorAbout">
-            <?= $author ?>
-        </div>
+        <div id="author">
+            <div id="picAuthor">
+                <img src="{{asset('images/me.png')}}" alt="author"/>
+            </div>
+            <div id="authorAbout">
+                <?= $author ?>
+            </div>
 
+        </div>
     </div>
-</div>
 @endsection
